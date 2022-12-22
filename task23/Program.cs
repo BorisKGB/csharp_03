ada40c7d73dc3d457a439a68acc606b34efb3bff
BorisKGB <flot3.sky@yandex.ru>
2022-12-22 22:34:38 +0300

    add task23 solution

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= num)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+    // Ну, или не используя библиотеку Math
+    // Console.Write($"{i*i*i} ");
+    i++;
+}
+
+Console.WriteLine();
